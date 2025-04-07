e62fd6595af3cbea25b11b66ddd60de8745a7972
Kobi Uzu <68594772+Uzu-Tech@users.noreply.github.com>
2025-04-07 18:19:19 +0400

    Add collision handling and improve rendering settings
    - Included "collison.h" for collision handling functions.
    - Enabled vertical synchronization for smoother rendering.
    - Updated FreeBody constructor to accept initial position, velocity, and restitution.
    - Added collision handling logic in the main loop for box and ball interactions.

--- Physics Engine/main.cpp
+++ Physics Engine/main.cpp
@@ -2,12 +2,14 @@
 #include "graphics.h"
 #include "config.h"
 #include "free_body.h"
+#include "collison.h"
 
 int main()
 {
     sf::RenderWindow window(
         sf::VideoMode({ Config::SCREEN_WIDTH, Config::SCREEN_HEIGHT }), "SFML works!"
     );
+    window.setVerticalSyncEnabled(true);
 
     sf::Vector2f box_pos(Config::BOX_BUFFER, Config::BOX_BUFFER);
     constexpr int BOX_WIDTH = Config::SCREEN_WIDTH - 2 * Config::BOX_BUFFER;
@@ -16,7 +18,7 @@ int main()
     Box box(box_pos, BOX_WIDTH, BOX_HEIGHT, Config::BOX_LINE_WIDTH);
     box.setColor(Config::WHITE);
 
-    FreeBody ball(Config::TEST_MASS, Config::CENTER);
+    FreeBody ball(Config::TEST_MASS, Config::INITIAL_POSITION, Config::INITIAL_VELOCITY, Config::RESTITUTION);
 
     sf::Clock clock;
     sf::Vector2f GRAVITY(0.0f, 981.0f);
@@ -29,6 +31,7 @@ int main()
         }
 
         ball.applyForce(ball.mass * GRAVITY);
+        handleBoxCollison(box, ball);
         float deltaTime = clock.restart().asSeconds();
         ball.update(deltaTime);
 
