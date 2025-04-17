a4f171bb237f19319fbfb94ef36e346189026446
Kobi Uzu <68594772+Uzu-Tech@users.noreply.github.com>
2025-04-17 16:33:02 +0400

    Refactor FreeBody class and enhance functionality
    - Removed `color` parameter from constructor; added `setColor` method for setting circle color.
    - Simplified `applyForce` method by defining it inline.
    - Introduced `getKinticEnergy` and `getGravitionalEnergy` methods for energy calculations.
    - Updated header file dependencies to include `vector_math.h` and `constants.h`.
    - Moved `getRadius` method for better organization; implementation unchanged.
    - Improved documentation for the `update` method to clarify its functionality.

--- Physics Engine/free_body.cpp
+++ Physics Engine/free_body.cpp
@@ -1,13 +1,12 @@
 #include "free_body.h"
 #include <algorithm>
 
-FreeBody::FreeBody(float mass, sf::Vector2f inital_pos, sf::Vector2f inital_vel, float restitution, sf::Color color)
+FreeBody::FreeBody(float mass, sf::Vector2f inital_pos, sf::Vector2f inital_vel, float restitution)
 	: mass(mass), position(inital_pos), velocity(inital_vel), restitution(restitution)
 {
 	circle = sf::CircleShape(mass, static_cast<std::size_t>(std::max(100.0f, mass)));
 	circle.setOrigin(sf::Vector2f(mass, mass));
 	circle.setPosition(position);
-	circle.setFillColor(color);
 }
 
 void FreeBody::update(float delta_time)
@@ -17,9 +16,4 @@ void FreeBody::update(float delta_time)
 	position = position + delta_time * velocity;
 	circle.setPosition(position);
 	acceleration = sf::Vector2f(0, 0);
-}
-
-void FreeBody::applyForce(sf::Vector2f force)
-{
-	acceleration += force / mass;
 }
--- Physics Engine/free_body.h
+++ Physics Engine/free_body.h
@@ -1,23 +1,25 @@
 #pragma once
 #include <SFML/Graphics.hpp>
-#include "config.h"
+#include "vector_math.h"
+#include "constants.h"
 
 class FreeBody : public sf::Drawable {
 public:
 	sf::Vector2f position, velocity, acceleration;
 	float mass, restitution;
 
-	FreeBody(
-		float mass,
-		sf::Vector2f inital_pos,
-		sf::Vector2f inital_vel = sf::Vector2f(0, 0),
-		float restitution = 1,
-		sf::Color color = Config::ACCENT_COLOR
-	);
-	void update(float delta_time);
-	void applyForce(sf::Vector2f force);
-
+	FreeBody(float mass, sf::Vector2f inital_pos, sf::Vector2f inital_vel = sf::Vector2f(0, 0), float restitution = 1);
+	void setColor(sf::Color color) { circle.setFillColor(color); };
 	float getRadius() const { return circle.getRadius(); };
+
+	// Updates position, velocity, and acceleration based on the laws of motion
+	void update(float delta_time);
+	void applyForce(sf::Vector2f force) { acceleration += force / mass; };
+
+	double getKinticEnergy() const { return (0.5 * mass * squared_magnitude(velocity) / 10'000); };
+	double getGravitionalEnergy(float floor_pos) const {
+		return (mass * Constants::GRAVITIONAL_FIELD_STRENGTH * ((floor_pos - position.y) / 100));
+	}
 
 private:
 	void draw(sf::RenderTarget& target, sf::RenderStates states) const override {
