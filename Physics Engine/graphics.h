a828ed51f79e195a26fc4ac88d16a4ee00ad9bfa
Kobi Uzu <68594772+Uzu-Tech@users.noreply.github.com>
2025-05-01 08:17:11 +0100

    Add Grid and Line classes; update Box class
    Introduced a new `Grid` class with a constructor for creating grids and a `setColor` method for color customization. Modified the `Box` class to include a constructor and updated its `setColor` method. Added a new `Line` class that supports both rounded and rectangular lines, along with a method to set the origin to the center. Updated the `getArrow` function and included `constants.h` for constant values used in the new classes.

--- Physics Engine/graphics.h
+++ Physics Engine/graphics.h
@@ -1,8 +1,31 @@
 #pragma once
 #include "free_body.h"
 #include "vector_math.h"
+#include "constants.h"
 #include <string>
+#include <vector>
 #include <SFML/Graphics.hpp>
+
+class Grid : public sf::Drawable
+{
+public:
+	Grid(float width, float height, float spacing, float y_start = 0.0f);
+	void setColor(sf::Color color);
+
+private:
+	void draw(sf::RenderTarget& target, sf::RenderStates states) const override
+	{
+		target.draw(lines, states);
+		target.draw(centerLine, states);
+	}
+	sf::VertexArray lines;
+
+	float width, height;
+	float spacing;
+	float y_start;
+	sf::Color color;
+	sf::RectangleShape centerLine; // Bold center
+};
 
 class Box : public sf::Drawable 
 {
@@ -18,6 +41,29 @@ private:
 		target.draw(points, states);
 	}
 	sf::VertexArray points;
+};
+
+class Line : public sf::Drawable, public sf::Transformable
+{
+public:
+	sf::Vector2f pos;
+	float length;
+	int line_width;
+	sf::Angle angle;
+
+	Line(sf::Vector2f pos, float length, int line_width, bool is_rounded = false, sf::Angle angle = sf::Angle::Zero);
+	void setOrginToCenter();
+
+private:
+	void draw(sf::RenderTarget& target, sf::RenderStates states) const override
+	{
+		// Apply the transform (from sf::Transformable)
+		states.transform *= getTransform();
+		target.draw(points, states);
+	}
+	sf::VertexArray points;
+	bool is_rounded;
+	sf::Vector2f orgin;
 };
 
 std::string round_to_string(double num, int decimal_places);
